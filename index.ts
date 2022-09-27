2e055aa4b8fab1d5a7416a499e247534f2737572
acai <acai@fi.uba.ar>
2022-09-27 16:23:58 -0300

    Example test

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
-import { GetSomething, GetUsers } from './src/controllers/user_controller';
+import { GetUsers, GetVehicles, GetDriverVehicles, CreateVehicle } from './src/controllers/user_controller';
 import { CONFIG } from './config';
 
 const app: Express = express();
@@ -12,13 +12,18 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.get('/api/something', GetSomething);
-
 app.get('/api/users', GetUsers);
 
+app.post('/api/vehicle', CreateVehicle);
 
-app.get('/', (req: Request, res: Response) => res.send("Fiuumber Server"));
+app.get('/api/vehicles', GetVehicles);
+
+app.get('/api/driver-vehicles', GetDriverVehicles);
+
+app.get('/', async (req: Request, res: Response) => res.send("Fiuumber Server :)"));
+
 
 app.listen(CONFIG.app.port, () => {
+  console.log("Cambio?");
   console.log(`⚡️[server]: Server is running at http://localhost:${CONFIG.app.port}`);
 });
--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -11,12 +11,56 @@ export const GetSomething = async (req: Request, res: Response) => {
   }
 };
 
-export const GetUsers = async (req: Request, res: Response) => {
+export const CreateVehicle = async (req: Request, res: Response) => {
   try {
     const prisma = new PrismaClient();
-    const users = await prisma.user.findMany();
-    res.status(200).send(users);
+
+    const { brand, model, image } = req.body
+
+    const body = await prisma.vehicle.create({
+      data: {
+        brand,
+        model,
+        image,
+      }
+    })
+
+    res.json(body).status(200);
   } catch (error) {
     res.status(500).send(error);
   }
 };
+
+export const GetUsers = async (req: Request, res: Response) => {
+  try {
+    const prisma = new PrismaClient();
+    const users = await prisma.user.findMany();
+    res.json(users).status(200);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export const GetVehicles = async (req: Request, res: Response) => {
+  try {
+    const prisma = new PrismaClient();
+    const vehicles = await prisma.vehicle.findMany();
+    res.json(vehicles);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export const GetDriverVehicles = async (req: Request, res: Response) => {
+  try {
+    const prisma = new PrismaClient();
+    const vehicles = await prisma.driverVehicle.findMany();
+    res.send(vehicles);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+
+
+
