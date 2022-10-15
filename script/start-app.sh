2de8c4fe3732faa7d2876076cf3e89cb4308e8a1
acai <acai@fi.uba.ar>
2022-10-15 19:19:46 -0300

    Add dockerfile

--- script/start-app.sh
+++ script/start-app.sh
@@ -4,6 +4,6 @@ npx prisma generate --schema=src/infraestructure/prisma/schema.prisma
 
 npx prisma migrate deploy --schema=src/infraestructure/prisma/schema.prisma
 
-RUN npm run build
+npm run build
 
 npm run start
