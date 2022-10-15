e3da2d784d99b13984591de8ff342083698ae069
acai <acai@fi.uba.ar>
2022-10-15 20:30:08 -0300

    Modify Dockerfile

--- script/start-app.sh
+++ script/start-app.sh
@@ -2,8 +2,6 @@
 
 npx prisma generate --schema=src/infraestructure/prisma/schema.prisma
 
-npx prisma migrate deploy --schema=src/infraestructure/prisma/schema.prisma
-
 npm run build
 
 npm run start
