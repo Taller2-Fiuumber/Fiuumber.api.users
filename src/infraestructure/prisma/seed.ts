83e24750454e7e81020ef53ea08a6f3afd83c871
Sofia Gualdieri <sgualdieri@fi.uba.ar>
2022-10-17 18:54:36 -0300

    Add command to dockerfile and fix bug in seed.ts

--- src/infraestructure/prisma/seed.ts
+++ src/infraestructure/prisma/seed.ts
@@ -1,9 +1,23 @@
 import { createPassenger } from '../../services/user-service';
+import { encrypt } from '../../utils/useful-functions';
+
+import { Passenger, PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
 
 const createPassengers = async () => {
+
+    const deletePassenger = await prisma.passenger.deleteMany();
+    const deleteUser = await prisma.user.deleteMany();
+
     console.log("Creating passengers...");
-    await createPassenger("juancho@fiuumber.com.ar", "Juan", "Perez", "Calle Falsa 123", "claveprivada$$", "juancho1234");
-    await createPassenger("elpapu@fiuumber.com.ar", "El", "Papu", "Calle Falsa 124", "claveprivada$$", "elpapu1234");
+    const encryptedPassword: string = await encrypt("juancho1234");
+    await createPassenger("juancho@fiuumber.com.ar", "Juan", "Perez", "Calle Falsa 123", "claveprivada$$", encryptedPassword);
+    const encryptedPassword2: string = await encrypt("elpapu1234");
+    await createPassenger("elpapu@fiuumber.com.ar", "El", "Papu", "Calle Falsa 124", "claveprivada$$", encryptedPassword2);
+
+
 };
 
 createPassengers();
