7fc93a7b883c334d95f5cc7641328cf27e6d804c
Jeremías <ramirezjeremias98@gmail.com>
2022-12-05 17:24:12 -0300

    add: wallet creation (#30)

---
+++ src/services/payments-service.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { CONFIG } from '../../config';
+
+export const HEADERS = { headers: { Accept: 'application/json'} };
+const URL_PAYMETNS = `${CONFIG.microservices.payments.url}${CONFIG.microservices.payments.basePath}`;
+
+export const createWallet = async (): Promise<string> => {
+  try {
+    const url = `${URL_PAYMETNS}/wallet`;
+    const response = await axios.post(url, HEADERS);
+    const { address } = response.data;
+    return address;
+  }
+  catch(ex) {
+    console.error("Error creating wallet " + ex);
+    throw ex;    
+  }
+};
