d3ad92e68966a3549d4e84558a29f106b6902d2f
facundo <facundomilhas@gmail.com>
2022-12-07 00:12:11 -0300

    Testing domain

---
+++ __tests__/services/payments-service.test.ts
@@ -0,0 +1,11 @@
+import {expect, test} from '@jest/globals';
+import { createWallet } from '../../src/services/payments-service';
+
+test('creates a new wallet', () => {
+
+
+    //const address = createWallet()   
+    //expect(adress).toBe();
+
+
+});
--- src/services/payments-service.ts
+++ src/services/payments-service.ts
@@ -16,3 +16,4 @@ export const createWallet = async (): Promise<string> => {
     throw ex;    
   }
 };
+
