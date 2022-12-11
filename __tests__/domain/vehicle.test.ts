07ddc409da839c3c0fdeb3ce2719544e9ffc56e7
Sofia Gualdieri <sgualdieri@fi.uba.ar>
2022-12-11 12:22:16 -0300

    test domain

---
+++ __tests__/domain/vehicle.test.ts
@@ -0,0 +1,16 @@
+import {expect, test} from '@jest/globals';
+import { Vehicle } from '../../src/domain/vehicle';
+
+test('creates a new vehivle', () => {
+
+    const vehicle = new Vehicle(
+        1,
+        'Toyota',
+        'Etios',
+        'img',
+        );
+    expect(vehicle.id).toBe(1);
+    expect(vehicle.brand).toBe('Toyota');
+    expect(vehicle.model).toBe('Etios');
+    expect(vehicle.image).toBe('img');
+});
