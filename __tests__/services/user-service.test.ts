45e8de5bf5193b3aaabe01f02658bcb13c11274a
Sofia Gualdieri <sgualdieri@fi.uba.ar>
2022-12-11 22:44:06 -0300

    mas tests de users

--- __tests__/services/user-service.test.ts
+++ __tests__/services/user-service.test.ts
@@ -295,73 +295,50 @@ test('should get driver vehicle by id', async () => {
 
 
 
-// test('should get all driver vehicles', async () => {
+test('should get all driver vehicles', async () => {
 
-//     const vehicle1 = {
-//         id:1,
-//         brand: 'Toyota',
-//         model: 'Etios',
-//         image : 'img',
-//         createdAt : new Date('2022-12-11T16:09:06.714Z'),
-//         updatedAt : new Date('2022-12-11T16:09:06.714Z'),
-//     }
-//     const vehicle2 = {
-//         id:2,
-//         brand: 'Toyota',
-//         model: 'Etios',
-//         image : 'img2',
-//         createdAt : new Date('2022-12-11T16:09:06.714Z'),
-//         updatedAt : new Date('2022-12-11T16:09:06.714Z'),
-//     }
+    const vehicle1 = {
+        id:1,
+        brand: 'Toyota',
+        model: 'Etios',
+        image : 'img',
+        createdAt : new Date('2022-12-11T16:09:06.714Z'),
+        updatedAt : new Date('2022-12-11T16:09:06.714Z'),
+    }
+    const vehicle2 = {
+        id:2,
+        brand: 'Toyota',
+        model: 'Etios',
+        image : 'img2',
+        createdAt : new Date('2022-12-11T16:09:06.714Z'),
+        updatedAt : new Date('2022-12-11T16:09:06.714Z'),
+    }
 
+    const driverVehicle1 = {
+        id: 1,
+        domain: 'ABC123',
+        modelYear: '2016',
+        colorName : 'Blue',
+        vehicleId: 1,
+        vehicle: vehicle1,
+        createdAt : new Date('2022-12-11T16:09:06.714Z'),
+        updatedAt : new Date('2022-12-11T16:09:06.714Z')
+    }
+    const driverVehicle2 ={
+        id: 2,
+        domain: 'ABC123',
+        modelYear: '2016',
+        colorName : 'Blue',
+        vehicleId: 1,
+        vehicle: vehicle2,
+        createdAt : new Date('2022-12-11T16:09:06.714Z'),
+        updatedAt : new Date('2022-12-11T16:09:06.714Z')
+   }
 
-
-//     prismaMock.driverVehicle.findMany.mockResolvedValue([
-//         {
-//             id: 1,
-//             domain: 'ABC123',
-//             modelYear: '2016',
-//             colorName : 'Blue',
-//             vehicleId: 1,
-//             vehicle: vehicle1,
-//             createdAt : new Date('2022-12-11T16:09:06.714Z'),
-//             updatedAt : new Date('2022-12-11T16:09:06.714Z')
-//         },
-//        {
-//         id: 2,
-//         domain: 'ABC123',
-//         modelYear: '2016',
-//         colorName : 'Blue',
-//         vehicleId: 1,
-//         vehicle: vehicle2,
-//         createdAt : new Date('2022-12-11T16:09:06.714Z'),
-//         updatedAt : new Date('2022-12-11T16:09:06.714Z')
-//    }
-//      ])
+    prismaMock.driverVehicle.findMany.mockResolvedValue([driverVehicle1,driverVehicle2])
  
-//      await expect(userServices.getDriverVehicles()).resolves.toEqual([
-//         {
-//             id: 1,
-//             domain: 'ABC123',
-//             modelYear: '2016',
-//             colorName : 'Blue',
-//             vehicleId: 1,
-//             vehicle: vehicle1,
-//             createdAt : new Date('2022-12-11T16:09:06.714Z'),
-//             updatedAt : new Date('2022-12-11T16:09:06.714Z')
-//         },
-//        {
-//         id: 2,
-//         domain: 'ABC123',
-//         modelYear: '2016',
-//         colorName : 'Blue',
-//         vehicleId: 1,
-//         vehicle: vehicle2,
-//         createdAt : new Date('2022-12-11T16:09:06.714Z'),
-//         updatedAt : new Date('2022-12-11T16:09:06.714Z')
-//    }
-//      ])
-// });
+     await expect(userServices.getDriverVehicles()).resolves.toEqual([driverVehicle1,driverVehicle2])
+});
 
 /*--------------------------------------User-----------------------------------*/
 
@@ -552,58 +529,298 @@ test('should get all users', async () => {
      ])
 });
 
-// test('should get user by account type ', async () => { 
- 
-//     prismaMock.user.findMany.mockResolvedValue([{
-//         id: 2,
-//         createdAt: new Date("2022-12-04T19:27:28.839Z"),
-//         updatedAt: new Date("2022-12-11T18:34:11.212Z"),
-//         lastLogin: new Date("2022-12-04T19:27:28.839Z"),
-//         accountType: AccountType.GOOGLE,
-//         email: "tomiomi@fi.uba.ar",
-//         password: "$2b$10$5FSVMtusNsz9mz.b2iSnQu/Mwvq6UD3oCrdECfYwLUnd1asIDyxFm",
-//         username: "anichucai",
-//         address: "San Juan 111",
-//         blocked: true,
-//         firstName: "Tomas",
-//         lastName: "Omi",
-//         walletAddress: "",
-//         notificationsToken: null,
-//         profile: Profile.PASSENGER
-//    },
-//    {
-//     id: 1,
-//     createdAt: new Date("2022-12-04T19:27:28.839Z"),
-//     updatedAt: new Date("2022-12-11T18:34:11.212Z"),
-//     lastLogin: new Date("2022-12-04T19:27:28.839Z"),
-//     accountType: AccountType.EMAIL,
-//     email: "tomiomi@fi.uba.ar",
-//     password: "$2b$10$5FSVMtusNsz9mz.b2iSnQu/Mwvq6UD3oCrdECfYwLUnd1asIDyxFm",
-//     username: "anichucai",
-//     address: "San Juan 111",
-//     blocked: true,
-//     firstName: "Tomas",
-//     lastName: "Omi",
-//     walletAddress: "",
-//     notificationsToken: null,
-//     profile: Profile.PASSENGER
-// }])
+//pasa pero esta rara
 
-//     await expect(userServices.getUsersByAccountType('EMAIL')).resolves.toEqual({
-//         id: 1,
-//         createdAt: new Date("2022-12-04T19:27:28.839Z"),
-//         updatedAt: new Date("2022-12-11T18:34:11.212Z"),
-//         lastLogin: new Date("2022-12-04T19:27:28.839Z"),
-//         accountType: AccountType.EMAIL,
-//         email: "tomiomi@fi.uba.ar",
-//         password: "$2b$10$5FSVMtusNsz9mz.b2iSnQu/Mwvq6UD3oCrdECfYwLUnd1asIDyxFm",
-//         username: "anichucai",
-//         address: "San Juan 111",
-//         blocked: true,
-//         firstName: "Tomas",
-//         lastName: "Omi",
-//         walletAddress: "",
-//         notificationsToken: null,
-//         profile: Profile.PASSENGER
-//     })
-// });
+test('should get user by account type ', async () => { 
+ 
+    const user = {
+        id: 2,
+        createdAt: new Date("2022-12-04T19:27:28.839Z"),
+        updatedAt: new Date("2022-12-11T18:34:11.212Z"),
+        lastLogin: new Date("2022-12-04T19:27:28.839Z"),
+        accountType: "EMAIL",
+        email: "tomiomi@fi.uba.ar",
+        password: "$2b$10$5FSVMtusNsz9mz.b2iSnQu/Mwvq6UD3oCrdECfYwLUnd1asIDyxFm",
+        username: "anichucai",
+        address: "San Juan 111",
+        blocked: true,
+        firstName: "Tomas",
+        lastName: "Omi",
+        walletAddress: "",
+        notificationsToken: null,
+        profile: Profile.PASSENGER
+    }
+
+    prismaMock.user.findMany.mockResolvedValue(user)
+
+    await expect(userServices.getUsersByAccountType('EMAIL')).resolves.toEqual({
+        id: 2,
+        createdAt: new Date("2022-12-04T19:27:28.839Z"),
+        updatedAt: new Date("2022-12-11T18:34:11.212Z"),
+        lastLogin: new Date("2022-12-04T19:27:28.839Z"),
+        accountType: "EMAIL",
+        email: "tomiomi@fi.uba.ar",
+        password: "$2b$10$5FSVMtusNsz9mz.b2iSnQu/Mwvq6UD3oCrdECfYwLUnd1asIDyxFm",
+        username: "anichucai",
+        address: "San Juan 111",
+        blocked: true,
+        firstName: "Tomas",
+        lastName: "Omi",
+        walletAddress: "",
+        notificationsToken: null,
+        profile: Profile.PASSENGER
+    })
+});
+
+
+test('should delete user by id ', async () => { 
+ 
+    const user = {
+                id: 2,
+                createdAt: new Date("2022-12-04T19:27:28.839Z"),
+                updatedAt: new Date("2022-12-11T18:34:11.212Z"),
+                lastLogin: new Date("2022-12-04T19:27:28.839Z"),
+                accountType: AccountType.EMAIL,
+                email: "tomiomi@fi.uba.ar",
+                password: "$2b$10$5FSVMtusNsz9mz.b2iSnQu/Mwvq6UD3oCrdECfYwLUnd1asIDyxFm",
+                username: "anichucai",
+                address: "San Juan 111",
+                blocked: true,
+                firstName: "Tomas",
+                lastName: "Omi",
+                walletAddress: "",
+                notificationsToken: null,
+                profile: Profile.PASSENGER
+            }
+
+    prismaMock.user.delete.mockResolvedValue(user)
+
+    await expect(userServices.deleteUserById(user.id)).resolves.toEqual({
+        id: 2,
+        createdAt: new Date("2022-12-04T19:27:28.839Z"),
+        updatedAt: new Date("2022-12-11T18:34:11.212Z"),
+        lastLogin: new Date("2022-12-04T19:27:28.839Z"),
+        accountType: AccountType.EMAIL,
+        email: "tomiomi@fi.uba.ar",
+        password: "$2b$10$5FSVMtusNsz9mz.b2iSnQu/Mwvq6UD3oCrdECfYwLUnd1asIDyxFm",
+        username: "anichucai",
+        address: "San Juan 111",
+        blocked: true,
+        firstName: "Tomas",
+        lastName: "Omi",
+        walletAddress: "",
+        notificationsToken: null,
+        profile: Profile.PASSENGER
+    })
+});
+
+/*---------------------------------Passenger-----------------------------------*/
+
+
+test('should create new passenger ', async () => { 
+
+    const user = {
+        id: 2,
+        createdAt: new Date("2022-12-04T19:27:28.839Z"),
+        updatedAt: new Date("2022-12-11T18:34:11.212Z"),
+        lastLogin: new Date("2022-12-04T19:27:28.839Z"),
+        accountType: AccountType.EMAIL,
+        email: "tomiomi@fi.uba.ar",
+        password: "$2b$10$5FSVMtusNsz9mz.b2iSnQu/Mwvq6UD3oCrdECfYwLUnd1asIDyxFm",
+        username: "anichucai",
+        address: "San Juan 111",
+        blocked: true,
+        firstName: "Tomas",
+        lastName: "Omi",
+        walletAddress: "",
+        notificationsToken: null,
+        profile: Profile.PASSENGER
+    }
+    const passenger = {
+        userId: 2,
+        user : user
+    }
+
+    prismaMock.passenger.create.mockResolvedValue(passenger)
+
+    await expect(userServices.createPassenger(passenger.user.email,passenger.user.firstName,passenger.user.lastName, passenger.user.username,passenger.user.password,passenger.user.address,passenger.user.walletAddress,passenger.user.accountType)).resolves.toEqual({
+        userId: 2,
+        user : user
+    })
+});
+
+
+
+test('should update passenger ', async () => { 
+
+    const user = {
+        id: 2,
+        createdAt: new Date("2022-12-04T19:27:28.839Z"),
+        updatedAt: new Date("2022-12-11T18:34:11.212Z"),
+        lastLogin: new Date("2022-12-04T19:27:28.839Z"),
+        accountType: AccountType.EMAIL,
+        email: "tomiomi@fi.uba.ar",
+        password: "$2b$10$5FSVMtusNsz9mz.b2iSnQu/Mwvq6UD3oCrdECfYwLUnd1asIDyxFm",
+        username: "tomiomi",
+        address: "San Juan 111",
+        blocked: true,
+        firstName: "Tomas",
+        lastName: "Omi",
+        walletAddress: "",
+        notificationsToken: null,
+        profile: Profile.PASSENGER
+    }
+    const passenger = {
+        userId: 2,
+        user : user
+    }
+
+    prismaMock.passenger.update.mockResolvedValue(passenger)
+
+    await expect(userServices.updatePassenger(passenger.userId,passenger.user.email,passenger.user.firstName,passenger.user.lastName, passenger.user.username,passenger.user.password,passenger.user.address,passenger.user.walletAddress)).resolves.toEqual({
+        userId: 2,
+        user : user
+    })
+});
+
+test('should get passenger by id ', async () => { 
+ 
+    const user = {
+        id: 2,
+        createdAt: new Date("2022-12-04T19:27:28.839Z"),
+        updatedAt: new Date("2022-12-11T18:34:11.212Z"),
+        lastLogin: new Date("2022-12-04T19:27:28.839Z"),
+        accountType: AccountType.EMAIL,
+        email: "tomiomi@fi.uba.ar",
+        password: "$2b$10$5FSVMtusNsz9mz.b2iSnQu/Mwvq6UD3oCrdECfYwLUnd1asIDyxFm",
+        username: "tomiomi",
+        address: "San Juan 111",
+        blocked: true,
+        firstName: "Tomas",
+        lastName: "Omi",
+        walletAddress: "",
+        notificationsToken: null,
+        profile: Profile.PASSENGER
+    }
+    const passenger = {
+        userId: 2,
+        user : user
+    }
+
+    prismaMock.passenger.findUniqueOrThrow.mockResolvedValue(passenger)
+
+    await expect(userServices.getPassenger(user.id)).resolves.toEqual({
+        userId: 2,
+        user : user
+    })
+});
+
+
+test('should get all passengers ', async () => { 
+ 
+    const user1 = {
+        id: 1,
+        createdAt: new Date("2022-12-04T19:27:28.839Z"),
+        updatedAt: new Date("2022-12-11T18:34:11.212Z"),
+        lastLogin: new Date("2022-12-04T19:27:28.839Z"),
+        accountType: AccountType.EMAIL,
+        email: "tomiomi@fi.uba.ar",
+        password: "$2b$10$5FSVMtusNsz9mz.b2iSnQu/Mwvq6UD3oCrdECfYwLUnd1asIDyxFm",
+        username: "tomiomi",
+        address: "San Juan 111",
+        blocked: true,
+        firstName: "Tomas",
+        lastName: "Omi",
+        walletAddress: "",
+        notificationsToken: null,
+        profile: Profile.PASSENGER
+    }
+    
+    const passenger1= {
+        userId: 1,
+        user : user1
+    }
+
+    const user2 = {
+        id: 2,
+        createdAt: new Date("2022-12-04T19:27:28.839Z"),
+        updatedAt: new Date("2022-12-11T18:34:11.212Z"),
+        lastLogin: new Date("2022-12-04T19:27:28.839Z"),
+        accountType: AccountType.EMAIL,
+        email: "tomiomi2@fi.uba.ar",
+        password: "$2b$10$5FSVMtusNsz9mz.b2iSnQu/Mwvq6UD3oCrdECfYwLUnd1asIDyxFm",
+        username: "tomiomi",
+        address: "San Juan 111",
+        blocked: true,
+        firstName: "Tomas",
+        lastName: "Omi",
+        walletAddress: "",
+        notificationsToken: null,
+        profile: Profile.PASSENGER
+    }
+    
+    const passenger2 = {
+        userId: 2,
+        user : user2
+    }
+
+    prismaMock.passenger.findMany.mockResolvedValue([passenger1 ,passenger2])
+
+    await expect(userServices.getPassengers()).resolves.toEqual(
+        [passenger1 ,passenger2]
+    )
+});
+
+
+test('should count passengers ', async () => { 
+ 
+    const user1 = {
+        id: 1,
+        createdAt: new Date("2022-12-04T19:27:28.839Z"),
+        updatedAt: new Date("2022-12-11T18:34:11.212Z"),
+        lastLogin: new Date("2022-12-04T19:27:28.839Z"),
+        accountType: AccountType.EMAIL,
+        email: "tomiomi@fi.uba.ar",
+        password: "$2b$10$5FSVMtusNsz9mz.b2iSnQu/Mwvq6UD3oCrdECfYwLUnd1asIDyxFm",
+        username: "tomiomi",
+        address: "San Juan 111",
+        blocked: true,
+        firstName: "Tomas",
+        lastName: "Omi",
+        walletAddress: "",
+        notificationsToken: null,
+        profile: Profile.PASSENGER
+    }
+    
+    const passenger1= {
+        userId: 1,
+        user : user1
+    }
+
+    const user2 = {
+        id: 2,
+        createdAt: new Date("2022-12-04T19:27:28.839Z"),
+        updatedAt: new Date("2022-12-11T18:34:11.212Z"),
+        lastLogin: new Date("2022-12-04T19:27:28.839Z"),
+        accountType: AccountType.EMAIL,
+        email: "tomiomi2@fi.uba.ar",
+        password: "$2b$10$5FSVMtusNsz9mz.b2iSnQu/Mwvq6UD3oCrdECfYwLUnd1asIDyxFm",
+        username: "tomiomi",
+        address: "San Juan 111",
+        blocked: true,
+        firstName: "Tomas",
+        lastName: "Omi",
+        walletAddress: "",
+        notificationsToken: null,
+        profile: Profile.PASSENGER
+    }
+    
+    const passenger2 = {
+        userId: 2,
+        user : user2
+    }
+
+    prismaMock.passenger.count.mockResolvedValue(2)
+
+    await expect(userServices.amountOfPassengers()).resolves.toEqual(2)
+});
